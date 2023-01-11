a181e1edbb1e1a95568cdd51d3f67f3614248dc7
hoa0202 <67028730+hoa0202@users.noreply.github.com>
2023-01-11 16:17:47 +0900

    Create noetic_install.sh

---
+++ noetic_install.sh
@@ -0,0 +1,26 @@
+!#/bin/bash
+
+sudo apt-get update -y && sudo apt-get upgrade -y 
+
+sudo apt-get install -y lsb-release
+
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-get install -y curl
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get install -y ros-noetic-desktop-full
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt-get install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+sudo apt-get install -y python3-rosdep
+
+rosdep init
+
+rosdep update
